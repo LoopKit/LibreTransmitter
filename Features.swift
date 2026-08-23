6ce3cc7e5e963834197bf5e23258c1ad9a39f264
ksilive <62499718+ksilive@users.noreply.github.com>
2026-08-23 17:29:14 +0200

    Update Features.swift

--- Features.swift
+++ Features.swift
@@ -17,7 +17,7 @@ public final class Features {
     static public var glucoseSettingsRequireAuthentication = false
     static public var alarmSettingsViewRequiresAuthentication = false
     
-    static public var allowsEditingFactoryCalibrationData = false
+    static public var allowsEditingFactoryCalibrationData = true
     static public var allowOneMinuteReadings = false
     
     // Uses Vibration through apples audio api for glucose alarms. This could be considered an api abuse from apple's standpoint;
